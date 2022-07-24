d54bd92a85efef5c7c8881967234835dbc1459b8
Yossi Tzeel-Krupp <yossitz1302@gmail.com>
2022-07-24 22:08:11 +0300

    added comments and updated all files to get ready for submition

--- app.py
+++ app.py
@@ -7,41 +7,51 @@ app = Flask(
     template_folder='templates',
     static_folder='static'
 )
-
+#main window(part 3 - web aplication)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     is_string = True
     output = ""
+    #checking that the user sent information for the website
     if request.method == 'POST':
+        #checking if the input values are empty
         if request.form['v0'] == "" or request.form['alpha0'] == "" or request.form['h0'] == "":
-            output = "Please fill in all fields"
+            output = 'Please fill in all fields'
         else:
+            #getting the values of the input
             v0 = float(request.form['v0'])
             alpha = float(request.form['alpha0'])
             h = float(request.form['h0'])
+            #checking that the values are correct(as explained in the word file)
             if v0 <= 0 or alpha < 0 or h < 0:
-                output = "Please fill in all fields correctly"
-                is_string = True
+                #error message if not
+                output = 'Please fill in all fields correctly'
             else:
                 output = calc.calc(v0, alpha, h)
                 is_string = False
-        if type(output) == str:
-            is_string = True
-        return render_template("index.html", output=output, is_string=is_string)
+        return render_template('index.html', output=output, is_string=is_string)
     return render_template('index.html', output=output, is_string=is_string)
 
+#api(part 2 -  architecture)
 @app.route('/api', methods=['GET', 'POST'])
 def api():
+    #checking if the input values are not empty
     if request.args.get('v0') and request.args.get('alpha0') and request.args.get('h0'):
+        #getting the input values 
         v0 = float(request.args.get('v0'))
         alpha0 = float(request.args.get('alpha0'))
         h0 = float(request.args.get('h0'))
+        #check that the values are correct (as explained in the word file)
         if v0 <= 0 or alpha0 < 0 or h0 < 0:
-            return jsonify({"error": "Please fill in all fields correctly"})
-        return json.dumps(calc.calc(v0, alpha0, h0))
+            #error message if the input is not correct
+            return jsonify({'error': 'Please fill in all fields correctly'})
+        #return json export for the file
+        return jsonify(calc.calc(v0, alpha0, h0))
     else:
-        return json.dumps(calc.calc(1, 1, 1))
-if __name__ == "__main__":
+        #returning default answer if there is no input
+        return jsonify(calc.calc(1, 1, 1))
+if __name__ == '__main__':
+    #running the app
     app.run(
         host='0.0.0.0',
         port=5000,
--- calc.py
+++ calc.py
@@ -5,23 +5,30 @@ from scipy import constants
 # I infered that the stike height is always y1 = 0
 
 g = constants.g
+# The method gets the starting velocity, starting angale and starting height and returns the hit distance, hit height, hit velocity and hit angale as a dict
+
 
 def calc(v0, alpha, h):
+    # first option of answer for the quadratic equation
     t1 = ((2 * v0 * math.sin(math.radians(alpha))) + math.sqrt(math.pow(-2 *
           v0 * math.sin(math.radians(alpha)), 2) + 8 * g * h))/(2 * g)
+    # second option of answer for the quadratic equation
     t2 = ((2 * v0 * math.sin(math.radians(alpha))) - math.sqrt(math.pow(-2 *
           v0 * math.sin(math.radians(alpha)), 2) + 8 * g * h))/(2 * g)
+    # finding the bigger answer
     t = max(t1, t2)
+    # horizontal velocity
     vx = v0 * math.cos(math.radians(alpha))
+    # distance
     x = vx * t
+    # vertical velocity
     vy = v0 * math.sin(math.radians(alpha)) - (g * t)
+    # velocity at the hit
     v = math.sqrt(math.pow(vx, 2) + math.pow(vy, 2))
+    # hit angle
     alpha1 = arctan(vy / vx)
+    # dict to return
     output = {
-        'Hit distance': x, 'Hit height': 0, 'Hit velocity': v, 'Hit angles': math.degrees(alpha1)
+        'Hit distance': x, 'Hit height': 0, 'Hit velocity': v, 'Hit angle': math.degrees(alpha1)
     }
     return output
-
-
-if __name__ == "__main__":
-    print(calc(10, 45, 20))
